e6337dd10ad0942363aefe70683a80e6aaa9045f
Hilderal Térence <terencehilderal@gmail.com>
2021-02-22 16:05:10 +0100

    adding subtotal method to data class and fullfilled information in html temlate returned by htmlFormat method

--- public/classes/Datas.js
+++ public/classes/Datas.js
@@ -13,14 +13,21 @@ export class Datas {
         this.tva = tva;
         this.date = date;
     }
+    subtotal(price, quantity, tva) {
+        const tvaPercent = tva / 100; //20% = 0.2%
+        const totalTva = price * tvaPercent;
+        return (price + totalTva) * quantity; //total ttc
+    }
     htmlFormat() {
+        // calcul total TTC
+        const totalPrice = this.subtotal(this.price, this.quantity, this.tva);
         return `<div class="row p-5">
     <div class="col-md-6">
         <h2 class="text-left">LOGO</h2>
     </div>
     <div class="col-md-6 text-right">
-        <p class="font-weight-bold mb-1">xxxxxxxxxxxxxxx<span class="font-weight-normal">xxxxxxxxxx</span></p>
-        <p class="font-weight-bold mb-1">Date <span class="font-weight-normal">xxxxxxxxxxxxxxx</span></p>
+        <p class="font-weight-bold mb-1">${this.documentType === 'invoice' ? 'Invoice' : 'Estimate'}<span class="font-weight-normal">${Math.floor(Math.random() * 101)}</span></p>
+        <p class="font-weight-bold mb-1">Date <span class="font-weight-normal">${this.date.toLocaleDateString()}</span></p>
     </div>
 </div>
 
@@ -34,11 +41,41 @@ export class Datas {
 
     <div class="col-sm-6 text-right">
         <p class="font-weight-bold">Informations du client</p>
-        <p class="mb-1">Mr/Mme xxxxxxxxxxxxxxx</p>
-        <p class="mb-1">xxxxxxxxxxxxxxx</p>
-        <p>xxxxxxxxxxxxxxx</p>
-        <p>xxxxxxxxxxxxxxx</p>
-        <p>xxxxxxxxxxxxxxx</p>
+        <p class="mb-1">Mr/Mme ${this.firstName} ${this.lastName}</p>
+        <p class="mb-1">${this.address}</p>
+        <p>${this.zip}</p>
+        <p>${this.town}</p>
+        <p>${this.country}</p>
+    </div>
+</div>
+
+<div class="row p-5">
+    <div class="col-md-12">
+        <table class="table">
+        <thead>
+            <tr>
+            <th class="border-0 text-uppercase small font-weight-bold">Produit/Service</th>
+            <th class="border-0 text-uppercase small font-weight-bold">Prix unitaire HT</th>
+            <th class="border-0 text-uppercase small font-weight-bold">Quantité</th>
+            <th class="border-0 text-uppercase small font-weight-bold">Total HT</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <td>${this.product}</td>
+            <td>${this.price} € HT</td>
+            <td>${this.quantity}</td>
+            <td>${this.price * this.quantity} € HT</td>
+            </tr>
+        </tbody>
+        </table>
+    </div>
+</div>
+
+<div class="d-flex flex-row-reverse bg-light p-4">
+    <div class="py-3 px-5">
+        <div class="mb-2">TOTAL TTC</div>
+        <div class="h2 font-weight-light">${totalPrice.toFixed(2)} €</div>
     </div>
 </div>`;
     }
