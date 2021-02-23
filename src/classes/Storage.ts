b8b716cfc4136a7a9336314ca0dc21846f22a551
Hilderal Térence <terencehilderal@gmail.com>
2021-02-23 10:39:09 +0100

    adding a Storage class with a checkLocalStorage , to check if we had something in our localStorage , if not we create a empty array

---
+++ src/classes/Storage.ts
@@ -0,0 +1,11 @@
+export class Storage {
+	constructor() {}
+	static checkLocalStorage(): void {
+		if (localStorage.getItem('invoice') === null) {
+			localStorage.setItem('invoice', '[]');
+		}
+		if (localStorage.getItem('estimate') === null) {
+			localStorage.setItem('estimate', '[]');
+		}
+	}
+}
--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,7 @@
 import { FormInput } from './classes/FormInputs.js';
 
+import { Storage } from './classes/Storage.js';
+
 new FormInput();
+
+Storage.checkLocalStorage();
